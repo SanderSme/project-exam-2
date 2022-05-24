11c85e5772cd501605c2775e7d866a9ac351b384
Sander Smedbøl <sander.smedbol@stud.noroff.no>
2022-05-24 20:11:06 +0200

    js fix

--- js/blog-list.js
+++ js/blog-list.js
@@ -8,13 +8,12 @@ async function getPosts(offset) {
   try {
     const response = await fetch(`${API_URL}&offset=${offset}`);
     let postData = await response.json();
-    let totalPages = await response.headers.get("X-WP-TotalPages");
+    let totalPages = response.headers.get("X-WP-TotalPages");
     counter += 1;
     const buttonContainer = document.querySelector(".button-container");
     if (counter == totalPages) {
       buttonContainer.style.display = "none";
     }
-    // blogList.innerHTML = "";
     for (let i = 0; i < postData.length; i++) {
       const postTitle = postData[i].acf.hero_title;
       const postDate = postData[i].date.split("T")[0];
@@ -22,6 +21,8 @@ async function getPosts(offset) {
       const postImage = postData[i].acf.hero_image.url;
       const postImageAlt = postData[i].acf.hero_image.alt;
       const category = postData[i].acf.category;
+      const loading = document.querySelector('.lds-hourglass')
+      loading.style.display = 'none'
       blogList.innerHTML += `<div class="blog-post"><img src="${postImage}" alt="${postImageAlt}" class="blog-post-img" />
             <div class="blog-info-container">
               <h2 class="post-heading post-info">${postTitle}</h2>
--- js/script.js
+++ js/script.js
@@ -1,14 +1,66 @@
-const track = document.querySelector(".carousel-track");
-const slides = Array.from(track.children);
-console.log(slides);
-const nextBtn = document.querySelector(".next-button");
-const prevBtn = document.querySelector(".prev-button");
-const dotsNav = document.querySelector(".carousel-nav");
-const dots = Array.from(dotsNav.children);
+const API_URL =
+  "http://boatdatlife.flywheelsites.com/wp-json/wp/v2/posts?acf_format=standard&per_page=6";
+const carouselContent = document.querySelector(".carousel-track");
+
+async function getPosts() {
+  try {
+    const response = await fetch(API_URL);
+    let postData = await response.json();
+    return postData;
+  } catch (error) {
+    console.log("ERROOOOOR");
+  }
+}
+getPosts().then((postData) => {
+  for (let i = 0; i < postData.length; i+=2) {
+    const postTitle = postData[i].acf.hero_title;
+    const postImage = postData[i].acf.hero_image.url;
+    const postImageAlt = postData[i].acf.hero_image.alt;
+    const postCategory = postData[i].acf.category;
+    const postDate = postData[i].date.split("T")[0];
+    let counter = i + 1;
+    if (counter === 6) {
+      break
+    }
+    const postTitle2 = postData[counter].acf.hero_title;
+    const postImage2 = postData[counter].acf.hero_image.url;
+    const postImageAlt2 = postData[counter].acf.hero_image.alt;
+    const postDate2 = postData[counter].date.split("T")[0];
+    const postCategory2 = postData[counter].acf.category;
+    carouselContent.innerHTML += `<li class="carousel-slide current-slide">
+    <div class="post">
+      <img src="${postImage}" alt="${postImageAlt}" class="post-img" />
+      <div class="info-container">
+        <h2 class="post-heading post-info">${postTitle}</h2>
+        <h3 class="category post-info">${postCategory}</h3>
+        <p class="post-date post-info">Posted on: ${postDate}</p>
+        <a href="./blog-post.html?id=${postData[i].id}" class="button post-info"
+          >Read ></a
+        >
+      </div>
+    </div>
+    <div class="post">
+      <img src="${postImage2}" alt="${postImageAlt2}" class="post-img" />
+      <div class="info-container">
+        <h2 class="post-heading post-info">${postTitle2}</h2>
+        <h3 class="category post-info">${postCategory2}</h3>
+        <p class="post-date post-info">Posted on: ${postDate2}</p>
+        <a href="./blog-post.html?id=${postData[counter].id}" class="button post-info"
+          >Read ></a
+        >
+      </div>
+    </div>
+  </li>`
+  }
+
+  const slides = Array.from(carouselContent.children);
+  const nextBtn = document.querySelector(".next-button");
+  const prevBtn = document.querySelector(".prev-button");
+  const dotsNav = document.querySelector(".carousel-nav");
+  const dots = Array.from(dotsNav.children);
 
 const slideSize = slides[0].getBoundingClientRect();
 const slideWidth = slideSize.width;
-console.log(slideWidth);
 
 const setSlidePosition = (slide, index) => {
   slide.style.left = slideWidth * index + "px";
@@ -28,23 +80,23 @@ const updateDots = (currentDot, targetDot) => {
 };
 
 prevBtn.addEventListener("click", (e) => {
-  const currentSlide = track.querySelector(".current-slide");
+  const currentSlide = carouselContent.querySelector(".current-slide");
   const prevSlide = currentSlide.previousElementSibling;
   const currentDot = dotsNav.querySelector(".current-slide");
   const prevDot = currentDot.previousElementSibling;
   const prevIndex = slides.findIndex((slide) => slide === prevSlide);
-  moveToSlide(track, currentSlide, prevSlide);
+  moveToSlide(carouselContent, currentSlide, prevSlide);
   updateDots(currentDot, prevDot);
   hideShowArrows(slides, nextBtn, prevBtn, prevIndex);
 });
 
 nextBtn.addEventListener("click", (e) => {
-  const currentSlide = track.querySelector(".current-slide");
+  const currentSlide = carouselContent.querySelector(".current-slide");
   const nextSlide = currentSlide.nextElementSibling;
   const currentDot = dotsNav.querySelector(".current-slide");
   const nextDot = currentDot.nextElementSibling;
   const nextIndex = slides.findIndex((slide) => slide === nextSlide);
-  moveToSlide(track, currentSlide, nextSlide);
+  moveToSlide(carouselContent, currentSlide, nextSlide);
   updateDots(currentDot, nextDot);
   hideShowArrows(slides, nextBtn, prevBtn, nextIndex);
 });
@@ -66,33 +118,13 @@ dotsNav.addEventListener("click", (e) => {
   const targetDot = e.target.closest("button");
   if (!targetDot) return;
 
-  const currentSlide = track.querySelector(".current-slide");
+  const currentSlide = carouselContent.querySelector(".current-slide");
   const currentDot = dotsNav.querySelector(".current-slide");
   const targetIndex = dots.findIndex((dot) => dot === targetDot);
   const targetSlide = slides[targetIndex];
 
-  moveToSlide(track, currentSlide, targetSlide);
+  moveToSlide(carouselContent, currentSlide, targetSlide);
   updateDots(currentDot, targetDot);
   hideShowArrows(slides, nextBtn, prevBtn, targetIndex);
 });
-
-const API_URL =
-  "http://boatdatlife.flywheelsites.com/wp-json/wp/v2/posts?acf_format=standard&per_page=6";
-const carouselContent = document.querySelector(".carousel-track");
-
-async function getPosts() {
-  try {
-    const response = await fetch(API_URL);
-    let postData = await response.json();
-    console.log(postData);
-    for (let i = 0; i < postData.length; i++) {
-      const postTitle = postData[i].acf.hero_title;
-      const postImage = postData[i].acf.hero_image.url;
-      const postImageAlt = postData[i].acf.hero_image.alt;
-      const postDate = postData[i].date.split("T")[0];
-    }
-  } catch (error) {
-    console.log("ERROOOOOR");
-  }
-}
-getPosts();
+});
