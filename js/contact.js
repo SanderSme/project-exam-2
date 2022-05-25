b4f59d655429a22d5e03c6edd17542760c74db3e
Sander Smedbøl <sander.smedbol@stud.noroff.no>
2022-05-25 14:21:32 +0200

    cleaning and metatags

--- js/contact.js
+++ js/contact.js
@@ -1,44 +1,43 @@
 function validateForm() {
-    let name = document.forms['form-container']['name'].value;
-    let email = document.forms['form-container']['email'].value;
-    let subject = document.forms['form-container']['subject'].value;
-    let message = document.forms['form-container']['message'].value;
+  let name = document.forms["form-container"]["name"].value;
+  let email = document.forms["form-container"]["email"].value;
+  let subject = document.forms["form-container"]["subject"].value;
+  let message = document.forms["form-container"]["message"].value;
 
-    let atPosition = email.indexOf('@');
-    let dotPosition = email.lastIndexOf('.');
+  let atPosition = email.indexOf("@");
+  let dotPosition = email.lastIndexOf(".");
 
-    const nameError = document.getElementById('name-error');
-    const emailError = document.getElementById('email-error');
-    const subjectError = document.getElementById('subject-error');
-    const messageError = document.getElementById('message-error');
-    const submitted = document.querySelector('.submitted');
+  const nameError = document.getElementById("name-error");
+  const emailError = document.getElementById("email-error");
+  const subjectError = document.getElementById("subject-error");
+  const messageError = document.getElementById("message-error");
+  const submitted = document.querySelector(".submitted");
 
-    if (name < 5) {
-        nameError.innerHTML = 'Please fill in your name';
-      } else {
-        nameError.innerHTML = '';
-      }
-      if (subject.length < 15) {
-        subjectError.innerHTML = 'Subject must be atleast 15 characters';
-      } else {
-        subjectError.innerHTML = '';
-      }
-      if (
-        atPosition < 1 ||
-        dotPosition < atPosition + 2 ||
-        dotPosition + 2 >= email.length
-      ) {
-        emailError.innerHTML = 'Please enter correct E-Mail ID';
-      } else {
-        emailError.innerHTML = '';
-      }
-      if (message.length < 25) {
-        messageError.innerHTML = 'Message must be atleast 25 characters';
-        return false
-      } else {
-        messageError.innerHTML = '';
-      }
-      submitted.innerHTML = 'Form submitted';
-      return true;
-
+  if (name < 5) {
+    nameError.innerHTML = "Please fill in your name";
+  } else {
+    nameError.innerHTML = "";
+  }
+  if (subject.length < 15) {
+    subjectError.innerHTML = "Subject must be atleast 15 characters";
+  } else {
+    subjectError.innerHTML = "";
+  }
+  if (
+    atPosition < 1 ||
+    dotPosition < atPosition + 2 ||
+    dotPosition + 2 >= email.length
+  ) {
+    emailError.innerHTML = "Please enter correct E-Mail ID";
+  } else {
+    emailError.innerHTML = "";
+  }
+  if (message.length < 25) {
+    messageError.innerHTML = "Message must be atleast 25 characters";
+    return false;
+  } else {
+    messageError.innerHTML = "";
+  }
+  submitted.innerHTML = "Form submitted";
+  return true;
 }
--- js/header.js
+++ js/header.js
@@ -1,25 +1,23 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menuItems = document.querySelectorAll('.menu-item');
-const hamburger = document.querySelector('.hamburger');
-const closeIcon = document.querySelector('.fa-xmark');
-const menuIcon = document.querySelector('.fa-bars');
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const menuItems = document.querySelectorAll(".menu-item");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".fa-xmark");
+const menuIcon = document.querySelector(".fa-bars");
 
 function toggleMenu() {
-    if (hamburgerMenu.classList.contains("showMenu")) {
-        hamburgerMenu.classList.remove("showMenu");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    } else {
-        hamburgerMenu.classList.add("showMenu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    }
-};
+  if (hamburgerMenu.classList.contains("showMenu")) {
+    hamburgerMenu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    hamburgerMenu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
 
 hamburger.addEventListener("click", toggleMenu);
 
-menuItems.forEach(
-    function(menuItem) {
-        menuItem.addEventListener("click", toggleMenu);
-    }
-);
+menuItems.forEach(function (menuItem) {
+  menuItem.addEventListener("click", toggleMenu);
+});
